d7e7a26c18fc5be2261806ae0af0f61c2767ee8e
Apfelholz <149402866+Apfelholz@users.noreply.github.com>
2024-10-18 17:46:38 +0200

    Ausgabe des Texts mit Markirten Sprüngen

--- src/Hopser.java
+++ src/Hopser.java
@@ -1,27 +1,38 @@
+import java.util.ArrayList;
+
 public class Hopser {
     int nededjumps1 = 0;
     int nededjumps2 = 0;
     int currendPos1 = 0;
     int currendPos2 = 0;
+    ArrayList<Integer> pos1;
+    ArrayList<Integer> pos2;
 
     public int hopsen(int start1, int start2, int[] extractedNumbers){
         int win = 0;
+
+        pos1 = new ArrayList<>(extractedNumbers.length / 15);
+        pos2 = new ArrayList<>(extractedNumbers.length / 15);
         
         currendPos1 = start1;
         currendPos2 = start2;
         while (true){
 
+                pos1.addLast(currendPos1);
                 currendPos1 += extractedNumbers[currendPos1];
                 nededjumps1++;
                 if (currendPos1 > extractedNumbers.length-1){
                     win = 1;
-                    break;
                 }
 
+                pos2.addLast(currendPos2);
                 currendPos2 += extractedNumbers[currendPos2];
                 nededjumps2++;
                 if (currendPos2 > extractedNumbers.length-1){
                     win = 2;
+                }
+
+                if (win != 0){
                     break;
                 }
 
--- src/Texthopsen.java
+++ src/Texthopsen.java
@@ -2,7 +2,12 @@
 public class Texthopsen {
     public static void main(String[] args) throws Exception {
 
-        String dateipfad = "data\\hopsen5.txt";
+        String ANSI_RESET = "\u001B[0m";
+        String ANSI_RED = "\u001B[31m";
+        String ANSI_BLUE = "\u001B[34m";
+        String ANSI_PURPLE = "\u001B[35m";
+
+        String dateipfad = "data\\hopsen1.txt";
 
         FileReaderx fileReaderx = new FileReaderx();
         char[] extractedLetters = fileReaderx.redarray(dateipfad, 0);
@@ -10,36 +15,51 @@ public class Texthopsen {
         Converter converter = new Converter();
         int[] extractedNumbers = converter.lettersToNumbers(extractedLetters);
 
-        System.err.println("--------------------------------------------------------------------------------------------------------------------");
-
-        for (int i = 0; i < extractedLetters.length; i++){
-            System.out.print(extractedLetters[i]);
-        }
-        System.err.println();
-
-        System.err.println("--------------------------------------------------------------------------------------------------------------------");
-
-        for (int i = 0; i < extractedNumbers.length; i++){
-            System.out.print(extractedNumbers[i]);
-        }
-        System.err.println();
-
-        System.err.println("--------------------------------------------------------------------------------------------------------------------");
-
         Hopser hopser = new Hopser();
 
         int win = hopser.hopsen(0, 1, extractedNumbers);
 
+        System.out.println("--------------------------------------------------------------------------------------------------------------------");
+
+        for (int i = 0; i < extractedNumbers.length; i++){
+            System.out.print(extractedNumbers[i]);
+        }
+        System.out.println();
+
+        System.out.println("--------------------------------------------------------------------------------------------------------------------");
+
+        int posi1 = 0;
+        int posi2 = 0;
+        for (int i = 0; i < extractedLetters.length; i++) {
+            boolean pos1InRange = (posi1 < hopser.pos1.size());
+            boolean pos2InRange = (posi2 < hopser.pos2.size());
+
+            if (pos1InRange && pos2InRange && hopser.pos1.get(posi1) == i && hopser.pos2.get(posi2) == i) {
+                System.out.print(ANSI_PURPLE + extractedLetters[i] + ANSI_RESET);
+            } else if (pos1InRange && hopser.pos1.get(posi1) == i) {
+                System.out.print(ANSI_RED + extractedLetters[i] + ANSI_RESET);
+                if (posi1 < hopser.pos1.size()) posi1++;
+            } else if (pos2InRange && hopser.pos2.get(posi2) == i) {
+                System.out.print(ANSI_BLUE + extractedLetters[i] + ANSI_RESET);
+                if (posi2 < hopser.pos2.size()) posi2++;
+            } else {
+                System.out.print(extractedLetters[i]);
+            }
+        }
+        System.out.println();
+
+        System.out.println("--------------------------------------------------------------------------------------------------------------------");
+
         System.out.println("Der Text ist " + extractedNumbers.length + " Buchstaben lang.");
 
         if (win == 1){
-            System.out.println("Bella hat mit " + hopser.nededjumps1 + " Schprüngen gewonnen.");
-            System.out.println("Amira war auf Position " + (hopser.currendPos2+1) );
-            System.out.println("Bella war auf Position " + (hopser.currendPos1+1) );
+            System.out.println("Bella(1) hat mit " + hopser.nededjumps1 + " Schprüngen gewonnen.");
+            System.out.println("Amira(2) war auf Position " + (hopser.currendPos2+1) );
+            System.out.println("Bella(1) war auf Position " + (hopser.currendPos1+1) );
         } else if (win == 2){
-            System.out.println("Amira hat mit " + hopser.nededjumps2 + " Schprüngen gewonnen.");
-            System.out.println("Bella war auf Position " + (hopser.currendPos1+1) );
-            System.out.println("Amira war auf Position " + (hopser.currendPos2+1) );
+            System.out.println("Amira(2) hat mit " + hopser.nededjumps2 + " Schprüngen gewonnen.");
+            System.out.println("Bella(1) war auf Position " + (hopser.currendPos1+1) );
+            System.out.println("Amira(2) war auf Position " + (hopser.currendPos2+1) );
         }
     }
 }
