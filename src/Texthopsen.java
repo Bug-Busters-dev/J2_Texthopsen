0f3eec84bcca317faf6d1983d273dd3ae2ddd628
Apfelholz <149402866+Apfelholz@users.noreply.github.com>
2024-10-20 19:49:46 +0200

    Ausgabe des Textes mit Lerhzeichen und Satzeichen

--- src/Texthopsen.java
+++ src/Texthopsen.java
@@ -30,22 +30,28 @@ public class Texthopsen {
 
         int posi1 = 0;
         int posi2 = 0;
-        for (int i = 0; i < extractedLetters.length; i++) {
-            boolean pos1InRange = (posi1 < hopser.pos1.size());
-            boolean pos2InRange = (posi2 < hopser.pos2.size());
+        int irelewanteZeichen = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (!Character.isLetter(text.charAt(i))){
+                System.out.print(text.charAt(i));
+                irelewanteZeichen++;
+            } else{
+                boolean pos1InRange = (posi1 < hopser.pos1.size());
+                boolean pos2InRange = (posi2 < hopser.pos2.size());
 
-            if (pos1InRange && pos2InRange && hopser.pos1.get(posi1) == i && hopser.pos2.get(posi2) == i) {
-                System.out.print(ANSI_PURPLE + extractedLetters[i] + ANSI_RESET);
-                if (posi1 < hopser.pos1.size()) posi1++;
-                if (posi2 < hopser.pos2.size()) posi2++;
-            } else if (pos1InRange && hopser.pos1.get(posi1) == i) {
-                System.out.print(ANSI_RED + extractedLetters[i] + ANSI_RESET);
-                if (posi1 < hopser.pos1.size()) posi1++;
-            } else if (pos2InRange && hopser.pos2.get(posi2) == i) {
-                System.out.print(ANSI_BLUE + extractedLetters[i] + ANSI_RESET);
-                if (posi2 < hopser.pos2.size()) posi2++;
-            } else {
-                System.out.print(extractedLetters[i]);
+                if (pos1InRange && pos2InRange && hopser.pos1.get(posi1) == i-irelewanteZeichen && hopser.pos2.get(posi2) == i-irelewanteZeichen) {
+                    System.out.print(ANSI_PURPLE + text.charAt(i) + ANSI_RESET);
+                    if (posi1 < hopser.pos1.size()) posi1++;
+                    if (posi2 < hopser.pos2.size()) posi2++;
+                } else if (pos1InRange && hopser.pos1.get(posi1) == i-irelewanteZeichen) {
+                    System.out.print(ANSI_RED + text.charAt(i) + ANSI_RESET);
+                    if (posi1 < hopser.pos1.size()) posi1++;
+                } else if (pos2InRange && hopser.pos2.get(posi2) == i-irelewanteZeichen) {
+                    System.out.print(ANSI_BLUE + text.charAt(i) + ANSI_RESET);
+                    if (posi2 < hopser.pos2.size()) posi2++;
+                } else {
+                    System.out.print(text.charAt(i));
+                }
             }
         }
         System.out.println();
